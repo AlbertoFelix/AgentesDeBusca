dcaecd81a8bbb81e4717fe88134ab41ee4455e53
AlbertoFelix <alberto.dsfelix@gmail.com>
2018-12-01 17:09:45 -0300

    Algoritmo de busca em profundidade criado.

--- buscaProfundidade.py
+++ buscaProfundidade.py
@@ -1,1 +1,11 @@
 #Algoritmo responsável por fazer a busca em profundidade.
+
+def bPf(grafo, inicio, visitado=None):
+
+    if visitado is None:
+        visitado = set()
+
+    visitado.add(inicio)
+    for next in grafo[inicio] - visitado:
+        bPf(grafo, next, visitado)
+    return visitado
