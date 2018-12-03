96d6918e9c9d2717748ac943dbb30dc210d27a55
Daniel <daniel.junior.ifpb@gmail.com>
2018-12-03 14:26:23 -0300

    Codificando busca

--- BuscaEmLargura.py
+++ BuscaEmLargura.py
@@ -1,1 +1,17 @@
 from Grafo import grafo
+
+
+def busca_largura(graph, start, goal):
+    """Algoritmo de busca em largura
+        Retorna um generator contendo todos os caminhos encontados na busca"""
+    queue = [(start, [start])]
+    while queue:
+        (vertex, path) = queue.pop(0)
+        for next in set([i for i in graph.neighbors(vertex)]) - set(path):
+            if next == goal:
+                yield path + [next]
+            else:
+                queue.append((next, path + [next]))
+
+v = next(busca_largura(grafo,'Arad','Dobretu'))
+print(v)
